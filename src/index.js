b55480898faa23026c14bf8e76c1ec674c4f6ac1
chuck <charles2588@gmail.com>
2023-05-08 22:34:46 -0700

    Adding CORS to allow aitool.fyi to read

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const v1router = require("./v1/routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
-
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://aitool.fyi'
+}));
 app.use(bodyParser.json());
 app.use("/api/v1", v1router);
 
