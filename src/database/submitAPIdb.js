f0a2033b00dfed3af73d2bc71e09b213f0f05cd1
chuck <charles2588@gmail.com>
2023-05-10 01:41:26 -0700

    Updated the console logging to show tool added

--- src/database/submitAPIdb.js
+++ src/database/submitAPIdb.js
@@ -1,6 +1,6 @@
 const addNewPendingTool = (newToolRow) => {
     //Save it to the database
-    console.log("Saved to database"+newToolRow);
+    console.log("Saved to database "+newToolRow['toolName']);
 };
 
 module.exports = {
